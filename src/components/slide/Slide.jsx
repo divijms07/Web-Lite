9ce1f276bacdc33a5b4ad40790770dd70ddfbdf3
tushar4303 <padhytushar@protonmail.com>
2023-09-24 16:03:14 +0530

    some changes

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,27 +1,21 @@
-import React from 'react'
-import './Slide.scss';
-import  Slider  from 'infinite-react-carousel';
-// import CatCard from '../catCard/CatCard';
-// import { cards } from '../../data';
-
+import React from "react";
+import "./Slide.scss";
+import Slider from "infinite-react-carousel";
+import CatCard from "../catCard/CatCard";
+import { cards } from "../../data";
 
 const Slide = () => {
   return (
     <div className="slide">
       <div className="container">
-        <Slider dots >
-          <div><h3>1</h3></div>
-          <div><h3>2</h3></div>
-          <div><h3>3</h3></div>
-          <div><h3>4</h3></div>
-          <div><h3>5</h3></div>
-          {/* {cards.map(card => {
+        <Slider slidesToshow={5} arrowsScroll={5}>
+          {cards.map((card) => (
             <CatCard item={card} key={card.id} />
-          })} */}
-      </Slider>
+          ))}
+        </Slider>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Slide
+export default Slide;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import "./Home.scss"
-import Featured from '../../components/featured/Featured'
-import Quote from '../../components/quote/Quote'
-import Slide from '../../components/slide/Slide'
+import React from "react";
+import "./Home.scss";
+import Featured from "../../components/featured/Featured";
+import Quote from "../../components/quote/Quote";
+import Slide from "../../components/slide/Slide";
 
 const Home = () => {
-    return (
-        <div className='home'>
-            <Featured/>
-            <Quote />
-            <Slide />
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <Featured />
+      <Quote />
+      <Slide />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
